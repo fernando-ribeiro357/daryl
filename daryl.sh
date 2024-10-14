c4ae3c43df5af4f84445d617644f351ee797d3ba
Fernando Ribeiro <fernando.ribeiro357@gmail.com>
2024-10-14 03:17:12 -0300

    Removendo comentários

--- daryl.sh
+++ daryl.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-
-## PRÉ-REQUISITOS:
-
-## Instalando inteligência artificial Ollama (https://github.com/ollama/ollama):
-# $ curl -fsSL https://ollama.com/install.sh | sh
-# ou
-# $ docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
-
-## Instalando piper (https://github.com/rhasspy/piper):
-# Baixe o arquivo tar.gz para a plataforma do seu computador
-# https://github.com/rhasspy/piper/releases/tag/2023.11.14-2
-# $ tar -xvzf piper_arm64.tar.gz ~/opt/
-# $ ln -s ~/opt/piper/piper ~/bin/piper
-# Baixe a voz em português
-# https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/pt/pt_BR/faber/medium/pt_BR-faber-medium.onnx
-# https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/pt/pt_BR/faber/medium/pt_BR-faber-medium.onnx.json
-
-# TODO: instalar programa para processar a voz através do microfone para transformá-la em texto (https://github.com/openai/whisper)
-
 resposta=""
 
 if [ $# -gt 0 ]
