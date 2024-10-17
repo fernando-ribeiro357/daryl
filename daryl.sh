f9184c54f0ebf876615a102f0fe3df33251d03b5
Fernando Ribeiro <fernando.ribeiro357@gmail.com>
2024-10-17 02:02:32 -0300

    Alterando modelo do ollama para gemma2. Retirada fala da pergunta.

--- daryl.sh
+++ daryl.sh
@@ -4,18 +4,18 @@ resposta=""
 if [ $# -gt 0 ]
 then
     filename="/tmp/.resposta_`date +%s`"
-    curl -s http://localhost:11434/api/generate -d "{\"model\":\"llama3.1\",\"stream\":false,\"prompt\":\"${1}\"}" | jq .response > $filename
-    
-    printf %b "$1" | piper -m ~/opt/piper/pt_BR/pt_BR-faber-medium.onnx --output-raw | aplay -r 20050 -f s16_le -t raw -
-    
+    curl -s http://localhost:11434/api/generate -d "{\"model\":\"gemma2\",\"stream\":false,\"prompt\":\"${1}\"}" | jq .response > $filename
+   
     resposta="$(cat $filename | sed 's/\*//g')"
+
 else
-  resposta="Olá, meu nome é Dariu. Como posso te ajudar?"
+
+  resposta="Olá. Como posso te ajudar hoje?"
+
 fi
 
 printf %b "$resposta"
-printf %b "$resposta" | piper -m ~/opt/piper/pt_BR/pt_BR-faber-medium.onnx --output-raw | aplay -r 20050 -f s16_le -t raw -
-
+printf %b "$resposta" | piper -m /opt/piper/pt-BR/pt_BR-faber-medium.onnx --sentence_silence 0.5 --output_raw | aplay -r 20050 -Dplughw:1,0 -f S16_LE -t raw -
 # arecord -r 192000 -f s16_le --buffer-time=1 - | aplay --buffer-time=1
 
 
