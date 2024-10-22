88283b8c9807990c9b157597522827c305bd8fc4
Fernando Ribeiro <fernando.ribeiro357@gmail.com>
2024-10-22 04:26:29 -0300

    Melhorando interação do usuário com o prompt

--- daryl.py
+++ daryl.py
@@ -1,18 +1,23 @@
 #!/home/orangepi/daryl/venv/bin/python
 
+import time
 import os
 import re
 import asyncio
 from ollama import AsyncClient, ResponseError
 
-def responder_prompt():
+def prompt():
     ''' 
     Função para receber o prompt do usuário e fazer a 
     requisição assíncrona ao chat com o prompt fornecido 
     '''
-    prompt = input('Entre com o prompt: ')
-    asyncio.run(chat(prompt))
-    print('\n\n')
+    pergunta=input('#>> ')
+    if pergunta.upper() != 'SAIR':
+        asyncio.run(chat(pergunta))
+        prompt()
+
+    print("..")
+    time.sleep(2)
 
 
 def retira_aster(text):
@@ -22,28 +27,40 @@ def retira_aster(text):
     return re.sub('\*', '', text)
 
 
-async def chat(prompt):
+async def chat(pergunta):
     ''' 
     Função que implementa o cliente do Ollama  
     '''
-    message = {'role': 'user', 'content': prompt}
+    message = {'role': 'user', 'content': pergunta}
     try:
         async for part in await AsyncClient().chat(model='llama3.1', messages=[message], stream=True):
             saida = retira_aster(part['message']['content'])
             print(saida, end='', flush=True)
     except ResponseError as e:
-        print('Error:', e.error)
+        print('Error:', e.error)    
+    print("\n")
+    
 
+def testeira():
+    ''' Exibe o nome estilizado do programa na tela '''
+    print(""" 
+    ********************************************************************
+    *                                                                  *
+    *  █▀█ █▀▀ █▀█ █▀▀ █░█ █▄░█ ▀█▀ █▀▀   ▄▀█   █░░ █░░ ▄▀█ █▀▄▀█ ▄▀█  *
+    *  █▀▀ ██▄ █▀▄ █▄█ █▄█ █░▀█ ░█░ ██▄   █▀█   █▄▄ █▄▄ █▀█ █░▀░█ █▀█  *
+    *                                   (digite 'sair' para encerrar)  *
+    ******************************************************************** 
+          """)
 
 def main():
     '''
     Função principal que inicia o programa 
     '''
     os.system('clear')
-    responder_prompt()
+    testeira()
+    prompt()
 
 
 if __name__ == '__main__':
     main()
-
 
